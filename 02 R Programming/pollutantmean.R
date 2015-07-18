1cda323e11b51950ed3eea3d844bdc24de941373
Anoop Varughese Mathew <anoop.v.mathew@gmail.com>
2015-07-18 18:25:54 +0300

    Changed filename to match submission expectations. Program produces correct result.

--- 02 R Programming/pollutantmean.R
+++ 02 R Programming/pollutantmean.R
@@ -12,4 +12,21 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## Return the mean of the pollutant across all monitors list
     ## in the 'id' vector (ignoring NA values)
     ## NOTE: Do not round the result!
+    
+    ## generate filenames for data from specific monitors using the id vector
+    filenames = paste(directory,
+                      "/",
+                      formatC(id,digits=0,width=3,flag="0",mode="integer"),
+                      ".csv", 
+                      sep="")
+    
+    ## read the specific pollutant data from the file subset
+    ## looping through the id integer vector
+    x <- numeric()
+    for(f in filenames) {
+        x <- c(x, read.csv(f, header=TRUE, dec=".")[[pollutant]])
+    }
+    
+    ## calculate mean
+    mean(x[!is.na(x)])
 }

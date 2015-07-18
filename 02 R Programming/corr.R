de84d21da22a0459ae66b82372e8c736611a5a5e
Anoop Varughese Mathew <anoop.v.mathew@gmail.com>
2015-07-18 21:24:20 +0300

    Corr.R Program now produces expected output

---
+++ 02 R Programming/corr.R
@@ -0,0 +1,36 @@
+source("complete.R")
+
+corr <- function(directory, threshold = 0) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'threshold' is a numeric vector of length 1 indicating the
+    ## number of completely observed observations (on all
+    ## variables) required to compute the correlation between
+    ## nitrate and sulfate; the default is 0
+    
+    ## Return a numeric vector of correlations
+    ## NOTE: Do not round the result!
+    
+    c <- complete(directory)
+    t <- c[c$nobs > threshold,]
+    cv <- vector("numeric", length(t$id))
+    
+    if (length(t$id) > 0) {
+        filenames = paste(directory,
+                          "/",
+                          formatC(t$id,digits=0,width=3,flag="0",mode="integer"),
+                          ".csv", 
+                          sep="")
+        i = 1
+
+        for(f in filenames) {
+            x <- read.csv(f, header=TRUE, dec=".")
+            cv[i] <- cor(x[["nitrate"]],
+                         x[["sulfate"]], 
+                         use="pairwise.complete.obs")
+            i <- i+1
+        }
+    }
+    cv
+}
